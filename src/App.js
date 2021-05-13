e6aa19b9f763e13dbd6b90b8940f3f88e9cd3e79
vdtrung <vdtrung1706@github.com>
2021-05-13 16:23:19 +0700

    useState

--- src/App.js
+++ src/App.js
@@ -1,15 +1,13 @@
 import { render } from 'react-dom';
-import Pet from './Pet';
+import SearchParams from './SearchParams';
 
 const App = () => {
   return (
     <div>
       <h1>Adopt Me</h1>
-      <Pet name="Luna" animal="dog" breed="havanese" />
-      <Pet name="Pepper" animal="bird" breed="Cockatiel" />
-      <Pet name="Hanpyon" animal="human" breed="Mix" />
+      <SearchParams />
     </div>
   );
 };
 
-render(App, document.getElementById('root'));
+render(<App />, document.getElementById('root'));
