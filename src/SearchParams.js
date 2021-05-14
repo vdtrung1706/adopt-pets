929aaf964eb37628045f96e76bcc0ab941e00598
vdtrung <vdtrung1706@github.com>
2021-05-14 22:48:26 +0700

    add some tailwindcss

--- src/SearchParams.js
+++ src/SearchParams.js
@@ -26,16 +26,19 @@ const SearchParams = () => {
   }
 
   return (
-    <div className="search-params">
+    <div className="my-0 mx-auto w-11/12">
       <form
+        className="p-10 mb-10 rounded-lg bg-gray-200 shadow-lg flex flex-col justify-center items-center divide-y divide-gray-900"
         onSubmit={e => {
           e.preventDefault();
           requesttPets();
         }}
       >
-        <label htmlFor="location">
+        <label className="search-label" htmlFor="location">
           Location
           <input
+            className="w-60"
+            type="text"
             id="location"
             value={location}
             placeholder="Location"
@@ -43,9 +46,10 @@ const SearchParams = () => {
           />
         </label>
 
-        <label htmlFor="animal">
+        <label className="search-label" htmlFor="animal">
           Animal
           <select
+            className="w-60"
             id="animal"
             value={animal}
             onChange={e => setAnimal(e.target.value)}
@@ -60,9 +64,10 @@ const SearchParams = () => {
           </select>
         </label>
 
-        <label htmlFor="breed">
+        <label className="search-label" htmlFor="breed">
           Breed
           <select
+            className="w-60 disabled:opacity-50"
             disabled={!breeds.length}
             id="breed"
             onChange={e => setBreed(e.target.value)}
@@ -77,9 +82,10 @@ const SearchParams = () => {
           </select>
         </label>
 
-        <label htmlFor="theme">
+        <label className="search-label" htmlFor="theme">
           Theme
           <select
+            className="w-60"
             value={theme}
             onChange={e => setTheme(e.target.value)}
             onBlur={e => setTheme(e.target.value)}
@@ -90,7 +96,12 @@ const SearchParams = () => {
             <option value="mediumorchid">Medium Orchid</option>
           </select>
         </label>
-        <button style={{ backgroundColor: theme }}>Submit</button>
+        <button
+          className="rounded px-6 py-2 color text-white hover:opacity-50 border-none"
+          style={{ backgroundColor: theme }}
+        >
+          Submit
+        </button>
       </form>
       <Results pets={pets} />
     </div>
