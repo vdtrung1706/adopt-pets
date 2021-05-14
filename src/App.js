2b5383a44ccc235397bc5ca60da63dee838bb2ed
vdtrung <vdtrung1706@github.com>
2021-05-15 00:22:29 +0700

    lazy,Suspense

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
-import { useState, StrictMode } from "react";
+import { useState, StrictMode, lazy, Suspense } from "react";
 import { render } from "react-dom";
 import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
-import Details from "./Details";
-import SearchParams from "./SearchParams";
+const Details = lazy(() => import("./Details"));
+const SearchParams = lazy(() => import("./SearchParams"));
 import ThemeContext from "./ThemeContext";
 
 const App = () => {
@@ -10,19 +10,21 @@ const App = () => {
   return (
     <ThemeContext.Provider value={theme}>
       <div>
-        <Router>
-          <header>
-            <Link to="/">Adopt Me!</Link>
-          </header>
-          <Switch>
-            <Route path="/details/:id">
-              <Details />
-            </Route>
-            <Route path="/">
-              <SearchParams />
-            </Route>
-          </Switch>
-        </Router>
+        <Suspense fallback={<h1>loading route...</h1>}>
+          <Router>
+            <header>
+              <Link to="/">Adopt Me!</Link>
+            </header>
+            <Switch>
+              <Route path="/details/:id">
+                <Details />
+              </Route>
+              <Route path="/">
+                <SearchParams />
+              </Route>
+            </Switch>
+          </Router>
+        </Suspense>
       </div>
     </ThemeContext.Provider>
   );
